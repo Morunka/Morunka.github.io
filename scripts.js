ba2d199a67e2bae53e670c907c3115b9c3702296
Morunka <robloxrojvolf@gmail.com>
2025-03-24 15:09:23 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,13 +9,34 @@ window.addEventListener('load', () => {
 // Логика кнопки "О студии"
 const aboutBtn = document.getElementById('about-btn');
 const description = document.getElementById('description');
+const linksBtn = document.getElementById('links-btn');
+const links = document.getElementById('links');
 const body = document.body;
 
 aboutBtn.addEventListener('click', (e) => {
     e.preventDefault();
+    if (body.classList.contains('links-active')) {
+        body.classList.remove('links-active');
+        links.classList.add('hide');
+    }
     body.classList.toggle('active');
     aboutBtn.classList.toggle('active');
     if (!body.classList.contains('active')) {
         description.classList.add('hide');
+    }
+});
+
+// Логика кнопки "Связи"
+linksBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (body.classList.contains('active')) {
+        body.classList.remove('active');
+        aboutBtn.classList.remove('active');
+        description.classList.add('hide');
+    }
+    body.classList.toggle('links-active');
+    linksBtn.classList.toggle('active');
+    if (!body.classList.contains('links-active')) {
+        links.classList.add('hide');
     }
 });
