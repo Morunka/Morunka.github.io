f4e9e6d91f665b6d907eb72de2b9090c8dfda0c9
Morunka <robloxrojvolf@gmail.com>
2025-03-24 14:57:47 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,12 +15,7 @@ aboutBtn.addEventListener('click', (e) => {
     e.preventDefault();
     body.classList.toggle('active');
     aboutBtn.classList.toggle('active');
-    if (body.classList.contains('active')) {
-        description.style.display = 'block';
-    } else {
-        description.style.opacity = '0';
-        setTimeout(() => {
-            description.style.display = 'none';
-        }, 1000); // Соответствует времени анимации в CSS
+    if (!body.classList.contains('active')) {
+        description.classList.add('hide');
     }
 });
